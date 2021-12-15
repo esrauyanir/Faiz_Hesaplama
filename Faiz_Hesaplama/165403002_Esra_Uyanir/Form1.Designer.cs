5d320f422e92e6c3b6f2c7ec480800157f7178c1
esrauyanir <85838470+esrauyanir@users.noreply.github.com>
2021-12-15 17:48:17 +0300

    Initial commit

---
+++ Faiz_Hesaplama/165403002_Esra_Uyanir/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace _165403002_Esra_Uyanir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.aylar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.aylik_taksit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.taksit_anapara = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.taksit_faiz = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kalan_para = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı Soyadı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kredi Türü";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kredi Tutarı ( ₺ )";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(391, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Vade ( ay )";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(391, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Faiz ( % )";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(114, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(154, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(455, 50);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(154, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "İhtiyaç Kredisi",
+            "Taşıt Kredisi",
+            "Konut Kredisi"});
+            this.comboBox1.Location = new System.Drawing.Point(114, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(154, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(455, 23);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(154, 21);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(534, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.aylar,
+            this.aylik_taksit,
+            this.taksit_anapara,
+            this.taksit_faiz,
+            this.kalan_para});
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 141);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(615, 306);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // aylar
+            // 
+            this.aylar.HeaderText = "Aylar";
+            this.aylar.Name = "aylar";
+            this.aylar.ReadOnly = true;
+            // 
+            // aylik_taksit
+            // 
+            this.aylik_taksit.HeaderText = "Aylık Taksit";
+            this.aylik_taksit.Name = "aylik_taksit";
+            this.aylik_taksit.ReadOnly = true;
+            // 
+            // taksit_anapara
+            // 
+            this.taksit_anapara.HeaderText = "Taksidin Anapara Kısmı";
+            this.taksit_anapara.Name = "taksit_anapara";
+            this.taksit_anapara.ReadOnly = true;
+            this.taksit_anapara.Width = 150;
+            // 
+            // taksit_faiz
+            // 
+            this.taksit_faiz.HeaderText = "Taksidin Faiz Kısmı";
+            this.taksit_faiz.Name = "taksit_faiz";
+            this.taksit_faiz.ReadOnly = true;
+            this.taksit_faiz.Width = 130;
+            // 
+            // kalan_para
+            // 
+            this.kalan_para.HeaderText = "Kalan Anapara";
+            this.kalan_para.Name = "kalan_para";
+            this.kalan_para.ReadOnly = true;
+            this.kalan_para.Width = 110;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 459);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aylar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aylik_taksit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn taksit_anapara;
+        private System.Windows.Forms.DataGridViewTextBoxColumn taksit_faiz;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kalan_para;
+    }
+}
+
---
+++ Faiz_Hesaplama/165403002_Esra_Uyanir/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _165403002_Esra_Uyanir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            if (comboBox1.SelectedIndex == 0 || comboBox1.SelectedIndex == 1)
+            {
+                comboBox2.Items.Add("3");
+                comboBox2.Items.Add("6");
+                comboBox2.Items.Add("9");
+                comboBox2.Items.Add("12");
+                comboBox2.Items.Add("18");
+                comboBox2.Items.Add("24");
+            }
+            if (comboBox1.SelectedIndex == 2)
+            {
+                comboBox2.Items.Add("12");
+                comboBox2.Items.Add("24");
+                comboBox2.Items.Add("36");
+                comboBox2.Items.Add("48");
+                comboBox2.Items.Add("60");
+            }
+        }
+        
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double anapara, faiz, ayliktkst, faiztutar, taksitanapara;
+            int vade;
+
+            anapara = double.Parse(textBox2.Text);
+            faiz = double.Parse(textBox3.Text);
+            vade = int.Parse(comboBox2.Text);
+            faiz = faiz / 100;  //faizimizin yüzdesini aldık
+
+            int x = 0;
+
+            ayliktkst = anapara * ((faiz * Math.Pow((faiz + 1), vade)) / (Math.Pow((1 + faiz), vade) - 1));
+
+            dataGridView1.RowCount = vade;
+
+            for (int i = 0; i < vade; i++)
+            {
+                x = x + 1;
+
+                faiztutar = anapara * faiz;
+                taksitanapara = ayliktkst - faiztutar;
+                anapara = anapara - taksitanapara;
+
+                dataGridView1.Rows[i].Cells[0].Value = x;
+                dataGridView1.Rows[i].Cells[1].Value = Math.Round(ayliktkst, 2);
+                dataGridView1.Rows[i].Cells[2].Value = Math.Round(taksitanapara, 2);
+                dataGridView1.Rows[i].Cells[3].Value = Math.Round(faiztutar, 2);
+                dataGridView1.Rows[i].Cells[4].Value = Math.Round(anapara, 2);
+
+            }
+
+        }
+    }
+}
